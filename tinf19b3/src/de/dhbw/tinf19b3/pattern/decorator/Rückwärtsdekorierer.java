abb652dffedc6eae2f299670bc3c6872d6698323
Daniel Lindner <daniel.lindner@softwareschneiderei.de>
2021-12-06 15:41:40 +0100

    Zeitpunkte für Erweiterungen

--- tinf19b3/src/de/dhbw/tinf19b3/pattern/decorator/Rückwärtsdekorierer.java
+++ tinf19b3/src/de/dhbw/tinf19b3/pattern/decorator/Rückwärtsdekorierer.java
@@ -10,7 +10,11 @@ public class Rückwärtsdekorierer extends Datenübertragungsdekorierer {
 	@Override
 	public void sende(
 			String nachricht) {
+		// Vor der Endkomponente
+		
 		StringBuilder buffer = new StringBuilder(nachricht);
 		super.sende(buffer.reverse().toString());
+
+		// Nach der Endkomponente
 	}
 }
